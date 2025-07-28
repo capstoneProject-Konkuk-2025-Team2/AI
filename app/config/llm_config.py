be76cdb453a5f98a970c4617073ee81a3fd22507
gongPyeon <pyeonkang@naver.com>
2025-07-28 17:53:57 +0900

    fix(llm_config): OPEN_API_KEY 설정

--- app/config/llm_config.py
+++ app/config/llm_config.py
@@ -1,27 +1,15 @@
-from llmware.library import Library
-from llmware.retrieval import Query
-from llmware.models import ModelCatalog
-from llmware.configs import LLMWareConfig, MilvusConfig
-from app.services.mvp_chatbot import (
-    load_user_profile,
-    load_filtered_programs_from_folder,
-    filter_by_interest,
-    generate_llm_prompt
-)
 import os
+from dotenv import load_dotenv
+from openai import OpenAI
+from langchain_openai import ChatOpenAI
+from app.utils.constants.error_codes import ErrorCode
+from app.utils.app_exception import AppException
 
-# 초기 설정
-config = LLMWareConfig()
-config.set_active_db("sqlite")
-MilvusConfig().set_config("lite", True)
-config.set_vector_db("chromadb")
-USER_TABLE_PATH = "user.json"
+load_dotenv()
 
-# 라이브러리 및 모델 로딩
-library_name = "my_library"
-embedding_model = "mini-lm-sbert"
-llm_model_name = "bling-answer-tool"
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+if not OPENAI_API_KEY:
+    raise AppException(ErrorCode.NOT_FOUND_OPENAI_API_KEY)
 
-lib = Library().load_library(library_name)
-query_engine = Query(lib)
-model = ModelCatalog().load_model(llm_model_name)
+client = OpenAI(api_key=OPENAI_API_KEY)
+llm = ChatOpenAI(openai_api_key=OPENAI_API_KEY, temperature=0)
